35d29fb43b912ee0710e5d34fc66eb7e63ba7022
QuachNamLuong <quachnamluong@gmail.com>
2025-07-05 01:31:52 +0000

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+// src/app.js
+
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './router/router.js';
@@ -15,15 +17,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(helmet());
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(cors());
 
+// Routes
+app.use('/', router);
+
+// Error Handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: "Internal Server Error" });
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
 });
-
-// API route goes here BEFORE error handler
-app.use('/', router);
 
 export default app;
